440b641e0dfd385218084d2f1b5eb3b6b44466ff
Jason YiZhang Chen <jz@jacyzon.com>
2015-09-03 17:59:40 +0800

    table migration

---
+++ database/migrations/2015_09_03_092525_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->string('location');
+            $table->string('category_id')->index();
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->string('user_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
